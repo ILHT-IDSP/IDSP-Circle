b3ac25ef8a7f5d613bed766e9f46feb55f2a9b5f
hateNaldoz <naguilar2@my.bcit.ca>
2025-04-28 14:09:10 -0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div id='burger-wrapper'>
+		<header>
+			<h1 className='text-red-500 text-4xl'>SMASH BURGER</h1>
+			<h2 className='text-cyan-300 text-3xl'>IS THERE A COMPILER THAT COMPILES PYTHON INTO JAVASCRIPT?</h2>
+		</header>
+	</div>
+  );
+}
