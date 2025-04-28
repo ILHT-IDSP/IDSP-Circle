d21bfbd429f330a1df25be3232e3453b44be7606
hateNaldoz <naguilar2@my.bcit.ca>
2025-04-28 14:11:11 -0700

    naldouche branch commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
 		<header>
 			<h1 className='text-red-500 text-4xl'>SMASH BURGER</h1>
 			<h2 className='text-cyan-300 text-3xl'>IS THERE A COMPILER THAT COMPILES PYTHON INTO JAVASCRIPT?</h2>
+			<h3 className="text-red-800"> NALDOUCHES BRANCH </h3>
 		</header>
 	</div>
   );
