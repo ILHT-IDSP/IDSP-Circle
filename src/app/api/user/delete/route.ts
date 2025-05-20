c4d9457f121c78580ae8b5bb623e68ad78b0423e
hateNaldoz <naguilar2@my.bcit.ca>
2025-05-20 14:53:34 -0700

    fix delete profile

--- src/app/api/user/delete/route.ts
+++ src/app/api/user/delete/route.ts
@@ -9,6 +9,15 @@ export async function POST(req: Request) {
 
         const confirmedUserExist = await prisma.user.findUnique({where: {id: userId}});
         if (!confirmedUserExist) throw new Error("User does not exist already");
+
+        await prisma.circle.deleteMany({
+            where: {creatorId: confirmedUserExist.id},
+        });
+
+        await prisma.album.deleteMany({
+            where: {creatorId: confirmedUserExist.id},
+        });
+
         await prisma.user.delete({where: {id: confirmedUserExist?.id}});
         console.log(`Circle Account of: ${confirmedUserExist?.username} successfully deleted!`);
         return NextResponse.json({message: "Successfully Deleted!"}, {status: 200});
